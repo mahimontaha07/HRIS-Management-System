3882d457d117f8e4ff3ca609109b67b4e5893435
Fabiha Montaha Mahi <mahifabiha0167@gmail.com>
2026-07-15 02:00:47 +0600

    Add Table of Contents to README
    CREATE TABLE Departments(
    Department_ID INT PRIMARY KEY,
    Department_Name VARCHAR(100)
    );
    
    CREATE TABLE Employees(
    Employee_ID INT PRIMARY KEY,
    Name VARCHAR(100),
    Email VARCHAR(100),
    Phone VARCHAR(20),
    Department_ID INT,
    Position VARCHAR(100),
    FOREIGN KEY (Department_ID)
    REFERENCES Departments(Department_ID)
    );
    
    CREATE TABLE Attendance(
    Attendance_ID INT PRIMARY KEY,
    Employee_ID INT,
    Attendance_Date DATE,
    Status VARCHAR(20),
    FOREIGN KEY(Employee_ID)
    REFERENCES Employees(Employee_ID)
    );

---
+++ database_schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Departments(
+Department_ID INT PRIMARY KEY,
+Department_Name VARCHAR(100)
+);
+
+CREATE TABLE Employees(
+Employee_ID INT PRIMARY KEY,
+Name VARCHAR(100),
+Email VARCHAR(100),
+Phone VARCHAR(20),
+Department_ID INT,
+Position VARCHAR(100),
+FOREIGN KEY (Department_ID)
+REFERENCES Departments(Department_ID)
+);
+
+CREATE TABLE Attendance(
+Attendance_ID INT PRIMARY KEY,
+Employee_ID INT,
+Attendance_Date DATE,
+Status VARCHAR(20),
+FOREIGN KEY(Employee_ID)
+REFERENCES Employees(Employee_ID)
+);
